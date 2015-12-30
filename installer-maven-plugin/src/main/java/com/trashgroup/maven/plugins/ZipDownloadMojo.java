36dcf0dab718f7191d6c0576c082d2309a39a371
Sebastian Haehnel <sebastian.haehnel@gmail.com>
2015-12-30 21:33:43 +0100

    add maven plugin plugin ans set requiresProject to false

--- installer-maven-plugin/src/main/java/com/trashgroup/maven/plugins/ZipDownloadMojo.java
+++ installer-maven-plugin/src/main/java/com/trashgroup/maven/plugins/ZipDownloadMojo.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.Properties;
 
 
-@Mojo(name = "install")
+@Mojo(name = "install", requiresProject = false)
 public class ZipDownloadMojo extends AbstractMojo {
 
     @Component
